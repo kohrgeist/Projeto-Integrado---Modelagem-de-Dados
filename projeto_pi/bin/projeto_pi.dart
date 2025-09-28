9489647af3cdbe5ad4221e2d2a808aac2f1e4369
sisaque2 <isaqued359@gmail.com>
2025-09-28 00:09:00 +0000

    exclusão de dados mortos main e teste

--- projeto_pi/bin/projeto_pi.dart
+++
@@ -1,5 +0,0 @@
-import 'package:projeto_pi/projeto_pi.dart' as projeto_pi;
-
-void main(List<String> arguments) {
-  print('Hello world: ${projeto_pi.calculate()}!');
-}
--- projeto_pi/test/projeto_pi_test.dart
+++ projeto_pi/test/projeto_pi_test.dart
@@ -1,8 +1,1 @@
-import 'package:projeto_pi/projeto_pi.dart';
-import 'package:test/test.dart';
 
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
