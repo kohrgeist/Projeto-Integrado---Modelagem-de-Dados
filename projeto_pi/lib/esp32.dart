14812df19b40d1acba6d732f5c44c7506fe3522a
Marcos Pereira <marcos.pereira.csdev@gmail.com>
2025-09-27 21:46:05 -0300

    alteração da classe esp32

--- projeto_pi/lib/esp32.dart
+++ projeto_pi/lib/esp32.dart
@@ -1,41 +1,40 @@
 import 'sensor_corrente.dart';
 import 'registro_leitura.dart';
-import '../services/gerenciador_dados.dart';
+import 'gerenciador_dados.dart';
 
-/// Representa o dispositivo ESP32, que contém um sensor de corrente (Composição).
+// Representa o dispositivo ESP32 que será usado para o projeto
 class Esp32 {
   final int _idEsp;
   final String _nome;
-  // O sensor é parte integrante do ESP32 (Composição).
-  // É final, pois não pode ser trocado após a criação do ESP32.
+  // É final, pq n pode ser trocado após a criação do esp32
   final SensorCorrente _sensorCorrente;
 
   Esp32({required int idEsp, required String nome})
       : _idEsp = idEsp,
         _nome = nome,
-        // O objeto SensorCorrente é instanciado DENTRO do construtor do Esp32.
+        // O objeto SensorCorrente é instanciado dentro do esp32
         _sensorCorrente = SensorCorrente(
-          idSensor: idEsp * 10, // ID do sensor derivado do ID do ESP
-          nome: 'Sensor de Corrente Interno',
-          unidade: 'A', // Ampere
+          idSensor: idEsp * 10, // ID do sensor derivado do ID do esp
+          nome: 'Sensor de Corrente 1',
+          unidade: 'A', // Ampére
         );
 
-  /// Calcula a potência instantânea (em Watts) com base na tensão fornecida.
-  /// Potência (W) = Tensão (V) * Corrente (A)
+  //aqui ele calcula a potência instantânea (em Watts) com base na tensão fornecida
+  //ele usa a seguinte fórmula: Potência (W) = Tensão (V) * Corrente (A)
   double calcularPotencia(double tensao) {
     final corrente = _sensorCorrente.lerValor();
     return tensao * corrente;
   }
 
-  /// Simula o processo de iniciar uma leitura completa e enviá-la.
+  // aqui existe uma simulação de processo de iniciar uma leitura completa e enviá-la
   void iniciarLeitura(GerenciadorDados gerenciador, {required double tensao}) {
     print('\nIniciando ciclo de leitura do ESP32 $_nome...');
     _sensorCorrente.exibirInfo();
     enviarDados(gerenciador, tensao: tensao);
   }
 
-  /// Cria um registro e o envia para o gerenciador de dados.
-  /// Demonstra a dependência da classe Esp32 com o GerenciadorDados.
+  // aqui ele cria um registro e o envia para o gerenciador de dados
+  // dependencia da classe esp com a gerenciador
   void enviarDados(GerenciadorDados gerenciador, {required double tensao}) {
     final potencia = calcularPotencia(tensao);
     
