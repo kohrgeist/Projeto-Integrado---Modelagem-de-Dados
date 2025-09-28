6065962beb70359d80b2a1a9c38c03e1d7c68570
sisaque2 <isaqued359@gmail.com>
2025-09-27 21:48:44 -0300

    Alteração no sensor.dart

--- projeto_pi/lib/sensor.dart
+++ projeto_pi/lib/sensor.dart
@@ -1,24 +1,21 @@
-/// Classe abstrata que define a estrutura base para um sensor.
-/// Utilizada para garantir que todos os sensores concretos implementem
-/// os métodos essenciais, aplicando o princípio do Polimorfismo.
+// Estrutura base para um sensor
 abstract class Sensor {
-  // Atributos privados para garantir o encapsulamento.
+  // Atributos para garantir o encapsulamento
   final int _idSensor;
   final String _nome;
   bool _ativo;
 
-  // Construtor da classe base.
-  Sensor(this._idSensor, this._nome, this._ativo);
+  // Método construtor
+  Sensor(this._idSensor, this._nome, this._tivo);
 
-  // Getters públicos para acesso controlado aos atributos privados.
+  // Requisições públicas para acesso controlado aos atributos privados
   int get idSensor => _idSensor;
   String get nome => _nome;
   bool get isAtivo => _ativo;
 
-  /// Método abstrato para ler o valor do sensor.
-  /// A implementação será específica para cada tipo de sensor.
+  /// Método abstrato para ler o valor do sensor
   double lerValor();
 
-  /// Método abstrato para exibir informações do sensor.
+  /// Exibir informações do sensor
   void exibirInfo();
 }
